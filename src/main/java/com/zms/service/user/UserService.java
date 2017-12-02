f3b48ca8e51e4526442ee6577c6f4ae122cf4b1d
v1olenceMS <sunboyoung@163.com>
2017-12-02 17:15:30 +0800

    将Service分层

--- src/main/java/com/zms/service/user/UserService.java
+++ src/main/java/com/zms/service/user/UserService.java
@@ -1,25 +1,82 @@
 package com.zms.service.user;
 
-import com.zms.dao.user.UserDao;
 import com.zms.pojo.user.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
 
 
-@Service
-public class UserService {
-    @Autowired
-    private UserDao dao;
+public interface UserService {
+    /**
+     * 创建用户
+     *
+     * @author zms
+     * @date 2017年11月27日下午5:50:38
+     */
+    public int createUser(User user);
 
-    @Transactional
-    public boolean addUser(User user){
-        boolean isAdd=dao.addUser(user);
-        return isAdd;
-    }
-    @Transactional
-    public boolean delUser(String name){
-        boolean isDel=dao.delUser(name);
-        return isDel;
-    }
+    /**
+     * 更新用户信息
+     *
+     * @author zms
+     * @date 2017年11月27日下午5:50:53
+     */
+    public void updateUser(User user);
+
+    /**
+     * 删除用户信息
+     *
+     * @author zms
+     * @date 2017年11月27日下午6:11:50
+     */
+    public void deleteUser(Long userId);
+
+    /**
+     * 添加用户--->角色信息
+     *
+     * @author zms
+     * @date 2017年11月27日下午6:11:57
+     */
+    public void correlationRoles(Long userId, Long roleIds);
+
+    /**
+     * 删除用户--->角色信息
+     *
+     * @author zms
+     * @date 2017年11月27日下午6:12:15
+     */
+    public void uncorrelationRoles(Long userId, Long roleIds);
+
+    /**
+     * 根据userId查出用户信息
+     *
+     * @author zms
+     * @date 2017年11月27日下午6:12:30
+     */
+    User findOne(Long userId);
+
+    /**
+     * 根据用户名查询用户信息
+     *
+     * @author zms
+     * @date 2017年11月27日下午6:12:50
+     */
+    User findByUsername(String userName);
+
+    /**
+     * 根据用户名查询角色信息
+     *
+     * @author zms
+     * @date 2017年11月27日下午6:13:05
+     */
+    Set<String> findRoles(String userName);
+
+    /**
+     * 根据用户名查询权限信息
+     *
+     * @author zms
+     * @date 2017年11月27日下午6:13:28
+     */
+    Set<String> findPermissions(String userName);
+
 }
+
---
+++ src/main/java/com/zms/service/user/UserServiceImpl.java
@@ -0,0 +1,65 @@
+package com.zms.service.user;
+
+import com.zms.dao.user.UserDao;
+import com.zms.pojo.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserDao userDao;
+
+    @Transactional(rollbackFor = Error.class)
+    public int createUser(User user) {
+        // TODO Auto-generated method stub
+        return userDao.createUser(user);
+    }
+
+    @Transactional(rollbackFor = Error.class)
+    public void updateUser(User user) {
+        // TODO Auto-generated method stub
+        userDao.updateUser(user);
+    }
+
+    @Transactional(rollbackFor = Error.class)
+    public void deleteUser(Long userId) {
+        // TODO Auto-generated method stub
+        userDao.deleteUser(userId);
+    }
+
+    @Transactional(rollbackFor = Error.class)
+    public void correlationRoles(Long userId, Long roleIds) {
+        // TODO Auto-generated method stub
+        userDao.correlationRoles(userId, roleIds);
+    }
+
+    @Transactional(rollbackFor = Error.class)
+    public void uncorrelationRoles(Long userId, Long roleIds) {
+        // TODO Auto-generated method stub
+        userDao.uncorrelationRoles(userId, roleIds);
+    }
+
+    public User findOne(Long userId) {
+        // TODO Auto-generated method stub
+        return userDao.findOne(userId);
+    }
+
+    public User findByUsername(String userName) {
+        // TODO Auto-generated method stub
+        return userDao.findByUsername(userName);
+    }
+
+    public Set<String> findRoles(String userName) {
+        // TODO Auto-generated method stub
+        return userDao.findRoles(userName);
+    }
+
+    public Set<String> findPermissions(String userName) {
+        // TODO Auto-generated method stub
+        return userDao.findPermissions(userName);
+    }
+}
