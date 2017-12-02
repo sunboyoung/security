27245a4d3574930994b63de074e95c8a015a0d04
v1olenceMS <sunboyoung@163.com>
2017-12-02 17:16:46 +0800

    permissionDao权限

---
+++ src/main/java/com/zms/dao/permission/PermissionDao.java
@@ -0,0 +1,24 @@
+package com.zms.dao.permission;
+
+import com.zms.pojo.permission.Permission;
+
+public interface PermissionDao {
+    /**
+     * 创建权限
+     *
+     * @param permission
+     * @return int
+     * @author zms
+     * @date 2017年11月30日下午1:57:29
+     */
+    public int createPermission(Permission permission);
+
+    /**
+     * 删除权限
+     *
+     * @param permissionId
+     * @author zms
+     * @date 2017年11月30日下午1:57:48
+     */
+    public void deletePermission(Long permissionId);
+}
