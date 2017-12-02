1455b5af7e3fa2c5ae589a1666666c5265644c12
v1olenceMS <sunboyoung@163.com>
2017-12-02 16:46:15 +0800

    自定义UserRealm 继承 AuthorizingRealm 获取AuthcationInfo以后交给父类调用Credentials验证(此处可以自定义实现) 最后获取AuthorizerInfo

---
+++ src/main/java/com/shiro/myrealm/UserRealm.java
@@ -0,0 +1,60 @@
+package com.shiro.myrealm;
+
+
+import com.util.SpringTool;
+import com.zms.pojo.user.User;
+import com.zms.service.user.UserService;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.ByteSource;
+import com.util.SpringTool;
+
+/**
+ *
+ * @Author zms
+ * @CreateTime 2017年11月30日下午2:25:23
+ * @Description 自定义用户域 继承AuthorizingRealm
+ *
+ */
+public class UserRealm extends AuthorizingRealm {
+
+    private UserService userService = (UserService) SpringTool.getBean("userService");
+
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        // TODO Auto-generated method stub
+        String username = (String)principals.getPrimaryPrincipal();
+        SimpleAuthorizationInfo authorizationInfo = new SimpleAuthorizationInfo();
+        authorizationInfo.setRoles(userService.findRoles(username));
+        authorizationInfo.setStringPermissions(userService.findPermissions(username));
+        return authorizationInfo;
+    }
+
+    @SuppressWarnings("unused")
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        // TODO Auto-generated method stub
+        String username = (String)token.getPrincipal();
+        User user = userService.findByUsername(username);
+        System.out.println(user.getUsername());
+        if(user == null) {
+            throw new UnknownAccountException();//没找到帐号
+        }
+        if(Boolean.TRUE.equals(user.getLocked())) {
+            throw new LockedAccountException(); //帐号锁定
+        }
+
+        //交给AuthenticatingRealm使用CredentialsMatcher进行密码匹配，如果觉得人家的不好可以自定义实现
+        SimpleAuthenticationInfo authenticationInfo = new SimpleAuthenticationInfo(
+                user.getUsername(), //用户名
+                user.getPassword(), //密码
+                ByteSource.Util.bytes(user.getUsername()),//salt=username+salt
+                getName()  //realm name
+        );
+        return authenticationInfo;
+    }
+}
