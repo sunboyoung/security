f5aa660034a4107fb12cacc3bff4d5d2fc95d6b2
v1olenceMS <sunboyoung@163.com>
2017-12-02 17:19:53 +0800

    userBean修改

--- src/main/java/com/zms/pojo/user/User.java
+++ src/main/java/com/zms/pojo/user/User.java
@@ -1,22 +1,91 @@
 package com.zms.pojo.user;
 
-public class User {
-    private  String name;
-    private  int age;
+import java.io.Serializable;
 
-    public String getName() {
-        return name;
+public class User implements Serializable {
+    private Long id;//用户id 主键
+    private String username;//用户名
+    private String password;//用户密码
+    private String salt;//加密盐
+
+    private Boolean locked = Boolean.FALSE; //用户是否被锁定
+
+
+    public User() {
+
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
     }
 
-    public int getAge() {
-        return age;
+    public Long getId() {
+        return id;
     }
 
-    public void setAge(int age) {
-        this.age = age;
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public Boolean getLocked() {
+        return locked;
+    }
+
+    public void setLocked(Boolean locked) {
+        this.locked = locked;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != null ? !id.equals(user.id) : user.id != null) return false;
+
+        return true;
+    }
+
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", salt='" + salt + '\'' +
+                ", locked=" + locked +
+                '}';
+    }
+
+    public String getCredentialsSalt() {
+        return this.username + this.salt;
     }
 }
