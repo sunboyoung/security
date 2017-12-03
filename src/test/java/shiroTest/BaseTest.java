fef0381db370d3fd427ecffbbc3ff0009a07a9ee
v1olenceMS <sunboyoung@163.com>
2017-12-03 19:17:32 +0800

    自定义Realm

---
+++ src/test/java/shiroTest/BaseTest.java
@@ -0,0 +1,67 @@
+package shiroTest;
+
+import com.zms.pojo.permission.Permission;
+import com.zms.pojo.role.Role;
+import com.zms.pojo.user.User;
+import com.zms.service.permission.PermissionService;
+import com.zms.service.role.RoleService;
+import com.zms.service.user.UserService;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.apache.shiro.util.ThreadContext;
+import org.junit.After;
+import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public abstract class BaseTest {
+
+    @Autowired
+    protected UserService userService;
+    @Autowired
+    protected RoleService roleService;
+    @Autowired
+    protected PermissionService permissionService;
+
+    protected String password = "123";
+
+    protected Permission p1;
+    protected Permission p2;
+    protected Permission p3;
+    protected Role r1;
+    protected Role r2;
+    protected User u1;
+    protected User u2;
+    protected User u3;
+    protected User u4;
+
+
+    @After
+    public void tearDown() throws Exception {
+        ThreadContext.unbindSubject();//退出时请解除绑定Subject到线程 否则对下次测试造成影响
+    }
+
+    protected void login(String configFile, String username, String password) {
+        //1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
+        Factory<SecurityManager> factory =
+                new IniSecurityManagerFactory(configFile);
+
+        //2、得到SecurityManager实例 并绑定给SecurityUtils
+        org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+
+        //3、得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+
+        subject.login(token);
+    }
+
+    public Subject subject() {
+        return SecurityUtils.getSubject();
+    }
+
+}
---
+++ src/test/java/shiroTest/UserRealmTest.java
@@ -0,0 +1,78 @@
+package shiroTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.zms.service.user.UserService;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.apache.shiro.util.ThreadContext;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:spring/spring-mvc.xml")
+public class UserRealmTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void testZms() {
+        if (userService == null) {
+            System.out.println("auto error");
+        } else {
+            System.out.println("auto success");
+        }
+    }
+
+
+    @Test
+    public void testLoginSuccess() {
+        login("classpath:realm/shiro.ini", "zhang", "z");
+        Assert.assertTrue(subject().isAuthenticated());
+    }
+
+    @Test
+    public void zms() {
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
+        subject.login(token);
+    }
+
+    protected void login(String configFile, String username, String password) {
+        //1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
+        Factory<org.apache.shiro.mgt.SecurityManager> factory =
+                new IniSecurityManagerFactory(configFile);
+
+        //2、得到SecurityManager实例 并绑定给SecurityUtils
+        org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
+
+        SecurityUtils.setSecurityManager(securityManager);
+
+        //3、得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+
+        subject.login(token);
+    }
+
+    public Subject subject() {
+        return SecurityUtils.getSubject();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        ThreadContext.unbindSubject();//退出时请解除绑定Subject到线程 否则对下次测试造成影响
+    }
+}
